d4fd375cbe6c7eb20a6d2bf61cb2ac09c130c479
Raiden <derpmaplemail@gmail.com>
2018-03-14 00:11:10 -0700

    Started Concentration game

--- Concentration/Concentration/ViewController.swift
+++ Concentration/Concentration/ViewController.swift
@@ -60,18 +60,5 @@ class ViewController: UIViewController {
         return emoji[card.identifier] ?? "?" // Return if not nil
         
     }
-    
-    
-    /*func flipCard(withEmoji emoji: String, on button: UIButton) {
-        if button.currentTitle == emoji {
-            button.setTitle("", for: UIControlState.normal)
-            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
-        } else {
-            button.setTitle(emoji, for: UIControlState.normal)
-            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-        }
-    }*/
-    
-    
 }
 
