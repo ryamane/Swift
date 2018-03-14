54b06b6d6e97390a7652234c864d498d2af661ee
Raiden <derpmaplemail@gmail.com>
2018-03-14 00:08:28 -0700

    Started Concentration game

--- Concentration/Concentration/ViewController.swift
+++ Concentration/Concentration/ViewController.swift
@@ -43,12 +43,12 @@ class ViewController: UIViewController {
                 button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
             } else {
                 button.setTitle("", for: UIControlState.normal)
-                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0) : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0) : #colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1)
             }
         }
     }
     
-    var emojiChoices = ["🎃", "👻", "❤️", "🙈", "👀", "👶🏻", "🤦🏻‍♂️", "✊🏻", "🤡"]
+    var emojiChoices = ["😦", "😏", "🤔", "😊", "😉", "🍟", "😶", "😱", "😢"]
     
     var emoji = [Int:String]() // This is a dictionary
     
